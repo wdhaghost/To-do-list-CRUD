97dcb5ea35959432adca8b0b90cffa462698bd0d
Coulibaly Abdourahmane <abdou.coulibaly@outlook.fr>
2022-09-07 14:10:08 +0200

    user story 1 done(display task from bdd)

---
+++ includes/_functions.php
@@ -0,0 +1,32 @@
+<?php 
+try {
+$dbCo = new PDO(
+'mysql:host=localhost:8889;dbname=to_do_list;charset=utf8',
+'php',
+'password'
+);
+$dbCo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_ASSOC);
+}
+catch (Exception $e) {
+    die("Unable to connect to the database.".$e->getMessage());
+    }
+
+function getTask($data){
+    $query= $data ->prepare("SELECT * FROM `task` WHERE 'done' =0 ") ;
+    $query->execute();
+    return $query->fetchAll();
+}
+
+
+function taskToHtml(array $array):string {
+$html="";
+foreach($array as $task){
+    $html.="<li id=".$task["id_task"]." class=\"task\"> <label for=\"task-".$task["id_task"]."\"></label>";
+    $html.="<input type=\"checkbox\" name=\"task-".$task["id_task"]."\" id=\"".$task["id_task"]."\">";
+    $html.="<div class=\"task-description\">";
+    $html.="<h3>".$task["description"]."</h3>";
+    $html.="<p>Rappel : ".$task["date_reminder"]."</p></div>";
+}
+return $html;
+}
+?>
--- index.php
+++ index.php
@@ -1,12 +1,41 @@
+<?php
+require_once "includes/_functions.php"
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
     <title>To Do List</title>
 </head>
 <body>
-    <
+    <div class="container">
+        <header class="header">
+            <h1 class="main-title">To do</h1>
+        </header>
+        <main class="">
+            <ul class="task-list">
+            
+                    <?php
+                    $result=getTask($dbCo);
+                    echo taskToHtml($result)?>
+                
+            </ul>
+            <?php
+                // echo "<pre>";
+                // var_dump($result);
+                // echo "</pre>";
+            ?>
+        </main>
+        
+
+
+
+
+
+    </div>
+    
 </body>
 </html>
